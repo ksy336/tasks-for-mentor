839f8a9e8d7212f36d3ad3e8ae56c44a1978114c
ksy336 <ksuoxy773@gmail.com>
2022-01-11 12:04:14 +0400

    feat: add class Circle

---
+++ Task_2.1.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Paint
+{
+     public class Circle
+    {
+        public double x;
+        public double y;
+        public double radius;
+        public const double pi = Math.PI;
+
+        public double GetCircle()
+        {
+            double length = 2 * pi * radius;
+            return length;
+        }
+        public double GetSquare()
+        {
+            double square = pi * (radius * radius);
+            return square;
+        }
+
+
+    }
+}
