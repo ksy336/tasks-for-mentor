b69a8012fbb65386f286415b94b7d4a95e7d211c
ksy336 <ksuoxy773@gmail.com>
2022-01-13 14:46:15 +0400

    feat: add GetStarted method

--- Task_2.1.2/CreationOfFigures.cs
+++ Task_2.1.2/CreationOfFigures.cs
@@ -1,47 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-public class CreationOfFigures
+namespace Custom_Paint
 {
-    int input;
-    string str;
-    public void GetStarted()
+    public class CreationOfFigures
     {
-        do
+        int input;
+        string str;
+        public void GetStarted()
         {
-            Console.WriteLine("Выберите действие:");
-            Console.WriteLine("1. Добавить фигуру");
-            Console.WriteLine("2. Вывести фигуры");
-            Console.WriteLine("3. Очистить холст");
-            Console.WriteLine("4. Выход");
-            str = Console.ReadLine();
-        } while (!int.TryParse(str, out input));
-        Console.Clear();
+            do
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1. Добавить фигуру");
+                Console.WriteLine("2. Вывести фигуры");
+                Console.WriteLine("3. Очистить холст");
+                Console.WriteLine("4. Выход");
+                str = Console.ReadLine();
+            } while (!int.TryParse(str, out input));
+            Console.Clear();
 
-        switch (input)
-        {
-            case 1:
-                AddFigure();
-                break;
-            case 2:
-                ShowFigures();
-                break;
-            case 3:
-                ClearAll();
-                break;
-            case 4:
-                Exit();
-                break;
-            default:
-                throw new NotImplementedException();
+            switch (input)
+            {
+                case 1:
+
+                    Console.WriteLine("Выбери фигуру для создания:");
+                    Console.WriteLine("1. Создать линию:");
+                    Console.WriteLine("2. Создать круг:");
+                    Console.WriteLine("3. Создать кольцо: ");
+                    Console.WriteLine("4. Создать треугольник: ");
+                    Console.WriteLine("5. Создать прямоугольник");
+                    Console.WriteLine("6. Создать квадрат: ");
+
+                    string variant = Console.ReadLine();
+                    Point point1;
+                    Point point2;
+                    Point point3;
+                    Point point4;
+                    double internalRadius;
+                    double externalRadius;
+                    switch(variant)
+                    {
+                        case 1:
+                            point1 = Input("Первая точка");
+                            point2 = input("Вторая точка");
+                            try
+                            {
+                                Side side = new Side(point1, point2);
+                                user.Figures.Add();
+                                Console.WriteLine($"Линия создана: {side}");
+                                Console.WriteLine();
+                                GetStarted();
+                            }
+                            catch
+                            {
+                                throw new Exception();
+                            }
+                    }
+                    break;
+                case 2:
+                    ShowFigures();
+                    break;
+                case 3:
+                    ClearAll();
+                    break;
+                case 4:
+                    Exit();
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
         }
+
+       
+
     }
 
-    public void AddFigure()
-    {
-        var figure = figureStarter.GetStartedFigure(type);
-        Console.WriteLine($"Фигура {figure} создана." + Environment.NewLine)
-              }
+
 
 }
-
-
