2c99138d2849a271a1be989b3a0d7ebafb950b8e
ksy336 <ksuoxy773@gmail.com>
2022-01-24 12:37:10 +0400

    My message

---
+++ Task_2.1.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using String;
+
+
+namespace Custom_Paint
+{
+    class Program   
+    {
+        static void Main(string[] args)
+        {
+           CreationOfFigures creationOfFigures = new CreationOfFigures();
+            creationOfFigures.GetAuthorised();
+            
+        }
+    }
+}
